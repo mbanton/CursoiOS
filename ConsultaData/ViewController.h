3f9e347121bb2d46501a169a5381c9d15502eb68
Aluno <Aluno@iMac-de-Aluno-2.local>
2013-09-14 10:53:06 -0300

    Initial Commit

---
+++ ConsultaData/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ConsultaData
+//
+//  Created by Aluno on 14/09/13.
+//  Copyright (c) 2013 Marcelo Anton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ConsultaData/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ConsultaData
+//
+//  Created by Aluno on 14/09/13.
+//  Copyright (c) 2013 Marcelo Anton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
