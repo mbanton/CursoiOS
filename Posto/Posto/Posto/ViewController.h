d7de676aed6b717e8204383158043adcc30d00ba
Marcelo Anton <marceloanton@gmail.com>
2013-09-28 05:59:15 -0300

    Init Posto project.

---
+++ Posto/Posto/Posto/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Posto
+//
+//  Created by Aluno on 28/09/13.
+//  Copyright (c) 2013 Marcelo Anton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Posto/Posto/Posto/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Posto
+//
+//  Created by Aluno on 28/09/13.
+//  Copyright (c) 2013 Marcelo Anton. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
